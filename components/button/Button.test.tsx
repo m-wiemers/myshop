9a1d0a3384a19f514e6e9e385203657a6bf09cbe
m-wiemers <marcel@klebauter.de>
2021-05-05 13:34:16 +0200

    ⚰️ remove buttontest

--- components/button/Button.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import { Primary } from "./Button.stories";
-
-it("renders the button in primary stat", () => {
-  render(<Primary {...Primary.args} />);
-  expect(screen.getByRole("button")).toHaveTextContent("Button");
-});
