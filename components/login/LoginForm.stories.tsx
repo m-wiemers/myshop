6328276fd3ffd4ae2f6653756941b0fafd89c7c5
m-wiemers <marcel@klebauter.de>
2021-05-10 16:51:53 +0200

    ♻️ resolve LoginForm Error

--- components/login/LoginForm.stories.tsx
+++ components/login/LoginForm.stories.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
-import LoginForm from "./LoginForm";
+import LoginForm, { LoginFormProps } from "./LoginForm";
 
 export default {
   title: "Common/Login",
   component: LoginForm,
 } as Meta;
 
-const Template: Story = () => <LoginForm />;
+const Template: Story<LoginFormProps> = (args) => <LoginForm {...args} />;
 
 export const Form = Template.bind({});
+Form.args = {
+  onClick: console.log("clicked"),
+  path: "#",
+  passwordChange: console.log("password changed"),
+  userNameChange: console.log("username changed"),
+  clickable: true,
+};
